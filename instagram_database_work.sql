39438cd036b4c8875d35497b2a95e80e3bef9a79
Pankaj-2003 <pankajtiwaribs@gmail.com>
2024-02-25 00:17:16 +0530

    compeleted the work

--- instagram_database_work.sql
+++ instagram_database_work.sql
@@ -6,7 +6,12 @@ select user_name from users order by created_at limit 5;
 
 -- one what day of week do most users register on?
 select * from users;
-SELECT count(*),DAYNAME(created_at) AS weekday_name from users group by weekday_name limit 2;
+SELECT 
+    COUNT(*), DAYNAME(created_at) AS weekday_name
+FROM
+    users
+GROUP BY weekday_name
+LIMIT 2;
 
 -- find the users who never posted a photo
 select user_name from users left join photos on users.id = photos.user_id where photos.id is null;
@@ -20,3 +25,18 @@ select ((select count(*) from photos) / (select count(*) from users));
 -- what are the top 5 commonly used hashtags?
 select * from tags ;
 select count(*) , tag_name from tags join photo_tags on tags.id = photo_tags.tag_id group by tag_id order by count(*) desc limit 5;
+
+-- find the users who have liked every single photo 
+select * from likes;
+SELECT 
+    user_name, user_id, COUNT(*) AS num_likes
+FROM
+    users
+        INNER JOIN
+    likes ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING num_likes = (SELECT 
+        COUNT(*)
+    FROM
+        photos);
+
